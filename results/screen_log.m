b3f6cbbd876f8b517ef6df6cf860c6592132ff00
r_liu <ruoshi98@github.com>
2018-07-12 15:47:28 -0400

    Sort and document files

--- results/screen_log.m
+++ results/screen_log.m
@@ -1,10 +1,9 @@
-log = importdata('/Users/ruoshiliu/Desktop/train_6-28/screen.log',',');
+log = importdata('/Users/ruoshiliu/Desktop/tests/d400_last2000/screen_6c.log',',');
 k = [];
 for i = 1:size(log,1)
     k = [k; strfind(log(i,:), 'total')];
 end
 loss_arr = [];
-loss_arr = []
 for i = 1:size(k,1)
     if ~isempty(cell2mat(k(i,1)))
         log_mat = cell2mat(log(i));
@@ -14,3 +13,9 @@ for i = 1:size(k,1)
     end
 end
 plot(loss_arr);
+
+x = [];
+for i = 1:690
+    x = [x mean(loss_arr((i-1)*116+1:i*116))];
+end
+plot(x);
